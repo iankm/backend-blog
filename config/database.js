7c9c8e54eaab14cd1ee52544d7407af1a0a50d06
iankm <ianmukherjee@gmail.com>
2021-10-22 21:42:06 -0400

    added new database configuraiton

--- config/database.js
+++ config/database.js
@@ -1,15 +1,39 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
+module.exports = () => ({
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {},
     },
   },
 });
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "bookshelf",
+//       settings: {
+//         client: "sqlite",
+//         filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
